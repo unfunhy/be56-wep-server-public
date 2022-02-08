743ccf096cb16511142629cda7cda04c98914e7c
unfunhy <unfunhy@gmail.com>
2022-02-08 19:52:38 +0900

    refactor: 5단계 구현 내용 변경 - Pattern, Matcher를 사용하여 기존 코드 수정 -> 매직 넘버 제거, 코드 직관성 항샹

--- src/main/java/framework/util/TemplateParser.java
+++ src/main/java/framework/util/TemplateParser.java
@@ -7,13 +7,23 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TemplateParser {
 
-    private final String INLINE_PATTERN = ".*\\{\\{.+}}.*";
-    private final String BLOCK_OPEN_PATTERN = ".*\\{\\{#.+}}.*";
-    private final String BLOCK_CLOSE_PATTERN = ".*\\{\\{\\/.+}}.*";
+    private final String INLINE_PATTERN_REGEX = ".*\\{\\{([a-zA-Z0-9]+)}}.*";
+    private final String BLOCK_OPEN_REGEX = ".*\\{\\{#([a-zA-Z0-9]+)}}.*";
+    private final String BLOCK_CLOSE_REGEX = ".*\\{\\{/([a-zA-Z0-9]+)}}.*";
+    private final String UNIT_INLINE_PATTERN_REGEX = "\\{\\{([a-zA-Z0-9]+)}}";
+    private final String UNIT_BLOCK_OPEN_REGEX = "\\{\\{#([a-zA-Z0-9]+)}}";
+    private final String UNIT_BLOCK_CLOSE_REGEX = "\\{\\{/([a-zA-Z0-9]+)}}";
     private final String NEW_LINE = "\r\n";
+
+    private final Pattern INLINE_PATTERN = Pattern.compile(UNIT_INLINE_PATTERN_REGEX);
+    private final Pattern BLOCK_OPEN_PATTERN = Pattern.compile(UNIT_BLOCK_OPEN_REGEX);
+    private final Pattern BLOCK_CLOSE_PATTERN = Pattern.compile(UNIT_BLOCK_CLOSE_REGEX);
+    private final int PATTERN_ARGS_INDEX = 1;
 
     public byte[] getModelAppliedView(File file, Model model) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(file);
@@ -22,86 +32,75 @@ public class TemplateParser {
 
         String line;
         while ((line = br.readLine()) != null) {
-            if (line.matches(BLOCK_OPEN_PATTERN)) {
+            if (line.matches(BLOCK_OPEN_REGEX)) {
                 line = replaceBlockTemplate(line, br, model);
-            } else if (line.matches(INLINE_PATTERN)) {
-                line = replaceInlineTemplate(line, model);
+            } else if (line.matches(INLINE_PATTERN_REGEX)) {
+                line = replaceInlineTemplate(line, model, null);
             }
             builder.append(line).append(NEW_LINE);
         }
         return builder.toString().getBytes(StandardCharsets.UTF_8);
     }
 
-    private String replaceInlineTemplate(String line, Model model) {
-        int idx = 0;
-        while (idx < line.length()) {
-            String target = getTemplateComponentName(line, idx, false);
-            if (target.length() <= 0) break;
-            idx = line.indexOf("}}", idx) + 1;
-            line = replaceTemplateComponent(line, target, model.getAttributes(target).toString());
+    /**
+     * target object에 해당하는 field가 없다면, model에 해당하는 값이 있는지 확인
+     */
+    private String replaceInlineTemplate(String line, Model model, Object targetObj) {
+        Matcher matcher = INLINE_PATTERN.matcher(line);
+        while (matcher.find()) {
+            String targetAttributeName = matcher.group(PATTERN_ARGS_INDEX);
+            String target = matcher.group();
+            String replacement = getValueFromModel(model, targetAttributeName, targetObj);
+            line = line.replace(target, replacement);
         }
         return line;
     }
 
     private String replaceBlockTemplate(String line, BufferedReader br, Model model) throws IOException {
-        String targetModelName = getTemplateComponentName(line, 0, true);
-        String blockLines = getAllHtmlElementsWrappedAsBlock(line, br);
+        Matcher blockMatcher = BLOCK_OPEN_PATTERN.matcher(line);
+        String targetComponentName = "";
+        if (blockMatcher.find()) {
+            targetComponentName = blockMatcher.group(PATTERN_ARGS_INDEX);
+        }
+        String blockLine = getAllHtmlElementsWrappedAsBlock(line, br);
 
         // model에 key, value가 있을 경우만 렌더링, 없으면 해당 부분 렌더링 하지 않음
-        Object target;
+        Object targetObj;
         try {
-            target = model.getAttributes(targetModelName);
-            checkModelAttributeExist(target);
+            targetObj = model.getAttributes(targetComponentName);
+            checkModelAttributeExist(targetObj);
         } catch (Exception e) {
             return "";
         }
 
         // list obj
-        if (target instanceof List) {
-            return getTableHtmlElements(model, (List<?>) target, blockLines);
+        if (targetObj instanceof List) {
+            return getTableHtmlElements(model, blockLine, (List<?>) targetObj);
         }
         // non-list obj
         else {
-            return replaceAllTemplateComponent(model, target, blockLines);
+            return replaceInlineTemplate(blockLine, model, targetObj);
         }
     }
 
-    private String getTableHtmlElements(Model model, List<?> targetList, String blockLines) {
+    private String getTableHtmlElements(Model model, String blockLine, List<?> targetList) {
         StringBuilder builder = new StringBuilder();
-        for (var item : targetList) {
-            builder.append(replaceAllTemplateComponent(model, item, blockLines));
+        for (var targetObj : targetList) {
+            String convertedLine = replaceInlineTemplate(blockLine, model, targetObj);
+            builder.append(convertedLine);
         }
         return builder.toString();
     }
 
-    private String replaceAllTemplateComponent(Model model, Object targetObj, String blockLines) {
-        int idx = 0;
-        while (idx < blockLines.length()) {
-            String targetFieldName = getTemplateComponentName(blockLines, idx, false);
-            if (targetFieldName.length() <= 0) break;
-            blockLines = replaceEachTemplateComponent(model, targetObj, blockLines, targetFieldName);
-            idx = blockLines.indexOf("}}", idx) + 1;
-        }
-        return blockLines;
-    }
-
-    /**
-     * target object에 해당하는 field가 없다면, model에 해당하는 값이 있는지 확인
-     * @param model controller로부터 받은 model
-     * @param targetObj 현재 템플릿 블럭이 참조하고 있는 오브젝트, 예를들어 {{#user}}라면 UserDto 객체
-     * @param blockLines 현재 템플릿 블럭으로 감싸여져있는 모든 html 내용물
-     * @param targetFieldName 타겟 오브젝트로부터 가져오고자 하는 필드 이름
-     * @return targetFieldName이 치환된 String 값을 리턴
-     */
-    private String replaceEachTemplateComponent(Model model, Object targetObj, String blockLines, String targetFieldName) {
+    private String getValueFromModel(Model model, String targetAttributeName, Object targetObj) {
         Object replacementObj;
         try {
-            replacementObj = getFieldValue(targetObj, targetFieldName);
+            replacementObj = getFieldValue(targetObj, targetAttributeName);
         } catch (Exception e) {
-            replacementObj = model.getAttributes(targetFieldName);
+            replacementObj = model.getAttributes(targetAttributeName);
         }
         checkModelAttributeExist(replacementObj);
-        return replaceTemplateComponent(blockLines, targetFieldName, replacementObj.toString());
+        return replacementObj.toString();
     }
 
     private Object getFieldValue(Object targetObj, String targetFieldName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
@@ -112,26 +111,15 @@ public class TemplateParser {
         return getter.invoke(targetObj, (Object[]) null);
     }
 
-    private String getTemplateComponentName(String line, int offset, boolean isBlockTemplate) {
-        int startIdx = line.indexOf("{{", offset);
-        int endIdx = line.indexOf("}}", offset);
-        if (startIdx < 0) return "";
-        startIdx += isBlockTemplate ? 3 : 2;
-        return line.substring(startIdx, endIdx);
-    }
-
     private String getAllHtmlElementsWrappedAsBlock(String line, BufferedReader br) throws IOException {
+        Matcher matcher = BLOCK_CLOSE_PATTERN.matcher(line);
         StringBuilder builder = new StringBuilder();
-        builder.append(line.replaceAll(BLOCK_OPEN_PATTERN, "")).append(NEW_LINE);
-        while (!line.matches(BLOCK_CLOSE_PATTERN) && (line = br.readLine()) != null) {
+        builder.append(line.replaceAll(BLOCK_OPEN_REGEX, "")).append(NEW_LINE);
+        while (!matcher.find() && (line = br.readLine()) != null) {
             builder.append(line).append(NEW_LINE);
+            matcher = BLOCK_CLOSE_PATTERN.matcher(line);
         }
-        return builder.toString().replaceAll(BLOCK_CLOSE_PATTERN, "");
-    }
-
-    private String replaceTemplateComponent(String line, String target, String replacement) {
-        target = String.format("{{%s}}", target);
-        return line.replace(target, replacement);
+        return builder.toString().replaceAll(BLOCK_CLOSE_REGEX, "");
     }
 
     private void checkModelAttributeExist(Object obj) {
